68e46d66870d0a1581bd2ce69f8b697c0842fdd4
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2021-04-05 14:36:53 -0300

    Gerador de blah blah blah

---
+++ src/ifpr/pgua/tads/geradorblahblahblah/Main.java
@@ -0,0 +1,20 @@
+package ifpr.pgua.tads.geradorblahblahblah;
+
+import java.io.IOException;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        Servidor servidor = new Servidor(12000);
+
+        servidor.processa();
+
+
+    }
+
+
+
+}
